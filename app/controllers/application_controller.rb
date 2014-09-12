d9f9d66c00fde9b3e03be40518d9b6faf2b32c01
Jeffrey Smith <jtsmith0107@gmail.com>
2014-09-12 00:57:47 -0700

    api keys and figaro

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  client = Twitter::REST::Client.new do |config|
+    config.consumer_key    = ENV['twitter_consumer_key']
+    config.consumer_secret = ENV['twitter_consumer_secret']
+  end
 end
