53c9a5b677f905bb994cf1227ff8ff759c3d6faf
Jeffrey Smith <jtsmith0107@gmail.com>
2014-09-12 14:34:50 -0700

    current user on root page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+batman = User.create({email: 'batman', password: 'batman'})
